88896d77d61715a842c1212497c3d7b1f9d3b337
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-22 23:11:20 +0400

    test

--- src/test/java/com/firstwebapp/YTWebApplicationTest.java
+++ src/test/java/com/firstwebapp/YTWebApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FirstWebAppApplicationTests {
+class YTWebApplicationTest {
 
     @Test
     void contextLoads() {
