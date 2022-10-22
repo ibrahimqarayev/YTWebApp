b70d7cda9adbb04eedd76da85f0d8e86f2385729
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-22 23:06:29 +0400

    Course

---
+++ src/main/java/com/firstwebapp/data/entity/Course.java
@@ -0,0 +1,35 @@
+package com.firstwebapp.data.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+@Column(name = "course_id")
+    private Integer id;
+
+    @Column(name = "course_name")
+    private String name;
+
+    @Column(name = "course_description")
+    private String description;
+
+    @ManyToMany(mappedBy = "courses")
+    private List<Student> students;
+
+
+}
