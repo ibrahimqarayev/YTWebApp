9203b16fb18731d2dfc11b58d9a13f3e0c586e99
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-22 23:10:24 +0400

    test

---
+++ src/main/java/com/firstwebapp/YTWebApplication.java
@@ -0,0 +1,13 @@
+package com.firstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YTWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YTWebApplication.class, args);
+    }
+
+}
