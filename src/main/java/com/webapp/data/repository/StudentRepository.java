1dd9deadfdd06f757640e41fcbb36392921f9cce
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-24 17:21:08 +0400

    StudentRepository

--- src/main/java/com/webapp/data/repository/StudentRepository.java
+++ src/main/java/com/webapp/data/repository/StudentRepository.java
@@ -1,8 +1,16 @@
 package com.webapp.data.repository;
 
 import com.webapp.data.entity.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends CrudRepository<Student, Integer> {
+
+    List<Student> findByName(String name);
+
+@Query(value = "select * from student where student_surname=?1",nativeQuery = true)
+    List<Student> getStudentBySurname(String surname);
 
 }
