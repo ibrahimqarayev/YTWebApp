fc1e0cef8399bd8fa33311c60d25c2e3c5062f63
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-25 14:59:06 +0400

    Student

--- src/main/java/com/webapp/data/entity/Student.java
+++ src/main/java/com/webapp/data/entity/Student.java
@@ -6,6 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 
@@ -19,12 +23,18 @@ import java.util.List;
 public class Student {
 
     @Id
-@Column(name = "student_id")
+    @Column(name = "student_id")
+    @Positive(message = "Musbet eded daxil edin")
+    @NotNull(message = "Id bos ola bilmez")
     private Integer id;
 
     @Column(name = "student_name")
+    @Size(min = 3,max = 30,message = "Ad minimim 3 max 30 simvol ola biler")
+    @NotBlank(message = "Ad bos ola bilmez")
     private String name;
 
+    @Size(min = 5,max = 30,message = "Soyad minimim 5 max 30 simvol ola biler")
+    @NotBlank(message = "Soyad bos ola bilmez")
     @Column(name = "student_surname")
     private String surname;
 
@@ -32,6 +42,6 @@ public class Student {
     @JoinTable(name = "students_courses",
             joinColumns = @JoinColumn(name = "student_id"),
             inverseJoinColumns = @JoinColumn(name = "course_id"))
-   private List<Course> courses;
+    private List<Course> courses;
 
 }
