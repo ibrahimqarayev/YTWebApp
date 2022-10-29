4a6f25679759dd551339fe4b6004a64bde3b655c
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-29 22:06:34 +0400

    delete

--- src/main/java/com/webapp/controller/TeacherController.java
+++
@@ -1,7 +0,0 @@
-package com.webapp.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class TeacherController {
-}
--- src/main/java/com/webapp/data/repository/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.webapp.data.repository;
-
-import com.webapp.data.entity.Teacher;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TeacherRepository extends CrudRepository<Teacher,Integer> {
-}
