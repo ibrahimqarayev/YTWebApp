bd604c5d2f4a7d07c02d591db51e1ef607856b69
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-01 16:00:13 +0400

    CourseController

--- src/main/java/com/webapp/controller/CourseController.java
+++ src/main/java/com/webapp/controller/CourseController.java
@@ -1,7 +1,34 @@
 package com.webapp.controller;
 
+import com.webapp.data.entity.Course;
+import com.webapp.data.repository.CourseRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/course")
 public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    //http://localhost:8080/student/index
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/list")
+    public String getAllCourses(Model model) {
+        Iterable<Course> courses = courseRepository.findAll();
+        model.addAttribute("courses", courses);
+        return "list";
+    }
+
+
 }
